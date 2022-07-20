01d82d317c90322cfc87236829728eef75f5eba6
Alejandro <alejandroverah97@gmail.com>
2022-07-20 12:39:59 -0500

    lazy loading

--- src/main.js
+++ src/main.js
@@ -22,7 +22,17 @@ MOVIE_URL_API = "movie/";
 
 //Utils
 
-function createMovies(movies, container) {
+const lazyLoader = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    // console.log(entry.target.setAttribute);
+    if (entry.isIntersecting) {
+      const url = entry.target.getAttribute("data-img");
+      entry.target.setAttribute("src", url);
+    }
+  });
+});
+
+function createMovies(movies, container, lazyLoad = false) {
   container.innerHTML = "";
 
   movies.forEach((movie) => {
@@ -37,9 +47,13 @@ function createMovies(movies, container) {
     movieImg.classList.add("movie-img");
     movieImg.setAttribute("alt", movie.title);
     movieImg.setAttribute(
-      "src",
+      lazyLoad ? "data-img" : "src",
       "https://image.tmdb.org/t/p/w300" + movie.poster_path
     );
+
+    if (lazyLoad) {
+      lazyLoader.observe(movieImg);
+    }
 
     movieContainer.appendChild(movieImg);
     container.appendChild(movieContainer);
@@ -72,7 +86,7 @@ async function getTrendingMoviesPreview() {
   const { data } = await api(TRENDING_URL_API);
   const movies = data.results;
 
-  createMovies(movies, trendingMoviesPreviewList);
+  createMovies(movies, trendingMoviesPreviewList, true);
 }
 
 async function getCategoriesPreview() {
